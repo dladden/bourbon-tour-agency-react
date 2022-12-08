a4234a2c1ed5aa4b004e852217ff98a80244f083
dladden <dladden77@gmail.com>
2022-12-08 15:46:06 -0500

    styling and logic for image slider

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -17,10 +17,66 @@ const ImageSlider = ({ slides }) => {
     backgroundSize: "cover",
     backgroundImage: `url(${slides[currentIndex].url})`,
   };
+  const leftArrowStyle = {
+    position: "absolute",
+    top: "50%",
+    transform: "translate(0, -50%)", //moving it to the half size of the element
+    left: "32px",
+    fontSize: "45px",
+    color: "#fff",
+    zIndex: 1,
+    cursor: "pointer",
+  };
+  const rightArrowStyle = {
+    position: "absolute",
+    top: "50%",
+    transform: "translate(0, -50%)", //moving it to the half size of the element
+    right: "32px",
+    fontSize: "45px",
+    color: "#fff",
+    zIndex: 1,
+    cursor: "pointer",
+  };
+
+  const dotsContainerStyle = {
+    display: "flex",
+    justifyContent: "center",
+  };
+  const dotsStyle = {
+    margin: "0 3px",
+    justifyContent: "center",
+  };
+
+  const goToPrevious = () => {
+    //checking if we are on the first slide
+    const isFirstSlide = currentIndex === 0;
+    //newIndex slides the length -1 even if its on first slide
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToNext = () => {
+    //checking if we are on the last slide
+    const isLastSlide = currentIndex === slides.length - 1;
+    //newIndex slides return to first slide 0 and if not go to slide +1
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
   return (
     <div style={sliderStyle}>
-      <div>❰</div>
+      {/* using simple utf 8 symbols for arrows */}
+      <div style={leftArrowStyle} onClick={goToPrevious}>
+        ❰
+      </div>
+      <div style={rightArrowStyle} onClick={goToNext}>
+        ❱
+      </div>
       <div style={slideStyle}></div>
+      <div>
+        {slides.map((slide, slideIndex) => {
+          <div key={slideIndex}>●</div>;
+        })}
+      </div>
     </div>
   );
 };
