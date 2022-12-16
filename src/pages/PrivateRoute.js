e0b7baf1a0c03250ee61d49731ffc683a9a3e0bc
dladden <dladden77@gmail.com>
2022-12-16 11:12:59 -0500

    setting up private route for checkout

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 // will remove later
-import { useUserContext } from '../context/user_context';
+import { useUserContext } from "../context/user_context";
+//Private Rout wraps the Checkout page this is done for private checkout and to prevent
+//user form accessing this page through the path
 
-const PrivateRoute = () => {
-  return <h4>Private Route</h4>;
+//TODO: see if user exists if yes: return the children - the wrapped component in this
+//case Checkout Page ("/checkout") if not: redirect the user to a login page
+
+//passing children and rest operator which passes the rest of the parameters from the wrap
+//Rest Operator vs Spread Operator: rest takes the rest of items, spread spreads items
+const PrivateRoute = ({ children, ...rest }) => {
+  const { tourUser } = useUserContext();
+
+  console.log(children);
+  console.log(rest); //rest of the children params :{} path:"/checkout" url:"/checkout" etc..
+
+  //returning the Route component from this code & spreading the "...rest" of the items
+  //using render function, if user exists pass the children if not return redirect home "/"
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return tourUser ? children : <Redirect to="/"></Redirect>;
+      }}
+    ></Route>
+  );
 };
 export default PrivateRoute;
